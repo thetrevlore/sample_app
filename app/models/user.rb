8431f92030b15eb2f6df7d1691674e7182bd77ce
Trevor Storey <trevor@photoshelter.com>
2019-11-13 22:46:05 -0700

    [modeling-users] add User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  # before_save { self.email = email.downcase! } # same as self.email.downcase, but inside the User model the 'self' keyword is optional ON THE RIGHT-HAND SIDE
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 end
